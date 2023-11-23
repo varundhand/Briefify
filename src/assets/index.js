2ef3ba4e0cdeb6e23ac889e445d895145b1c695c
Varun Dhand <varundhand77@gmail.com>
2023-11-23 19:36:21 +0530

    Add: assets

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import linkIcon from "./link.svg";
+import loader from "./loader.svg";
+import copy from "./copy.svg";
+import tick from "./tick.svg";
+import trash from "./trash.svg";
+
+export { linkIcon, loader, copy, tick, trash };
