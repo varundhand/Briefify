a35ff0e37f2303b471b2423799d1b9375e465a5d
Varun Dhand <varundhand77@gmail.com>
2023-11-26 03:09:51 +0530

    Fix: link svg style fix

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -71,14 +71,13 @@ const Demo = () => {
   //  console.log(allArticles)
   return (
     <div className='w-full flex justify-center'>
-      <section className="mt-10 w-full max-w-xl">
+      <section className="mt-16 w-full max-w-xl">
         <div className="flex flex-col w-full gap-2 ">
           <form 
             className="relative flex justify-center items-center"
             onSubmit={handleSubmit}  
           >
-            
-            <LinkSVG/>
+            <LinkSVG className="left-0 w-5 ml-3 my-2 absolute "/>
             <input 
               type="url" 
               placeholder='Enter a URL'
